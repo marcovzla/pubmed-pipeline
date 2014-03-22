31b6d3b93f1d23e2ded0b552f919673282e1cba4
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-03-22 12:44:14 -0700

    updated pipeline script

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+RAW_DIR="$1"
+RAW_FILES="${RAW_DIR}/*.txt"
+
+SS_DIR="${RAW_DIR}/../ss"
+[ ! -d $SS_DIR ] && mkdir $SS_DIR
+TOK_DIR="${RAW_DIR}/../tok"
+[ ! -d $TOK_DIR ] && mkdir $TOK_DIR
+PARSE_DIR="${RAW_DIR}/../parse"
+[ ! -d $PARSE_DIR ] && mkdir $PARSE_DIR
+SDEP_DIR="${RAW_DIR}/../sdep"
+[ ! -d $SDEP_DIR ] && mkdir $SDEP_DIR
+SDEPCC_DIR="${RAW_DIR}/../sdepcc"
+[ ! -d $SDEPCC_DIR ] && mkdir $SDEPCC_DIR
+TAG_DIR="${RAW_DIR}/../tag"
+[ ! -d $TAG_DIR ] && mkdir $TAG_DIR
+
 SRC_DIR="src"
 TLS_DIR="tls"
 EXT_DIR="$TLD_DIR/ext"
@@ -10,10 +26,22 @@ SDEP="$SRC_DIR/ptb_to_sdep.sh"
 GENIA="$SRC_DIR/tagger.sh"
 
 
+for f in $RAW_FILES; do
+    fname=$(basename $f)
 
-cat abstract.txt | $GENIASS > abstract.txt.ss
-cat abstract.txt.ss | $TOKENIZER > abstract.txt.tok
-cat abstract.txt.ss | $BLLIP > abstract.txt.ptb
-cat abstract.txt.ptb | $SDEP > abstract.txt.sdep
-cat abstract.txt.ptb | $SDEP -c > abstract.txt.sdepcc
-cat abstract.txt.ss | $GENIA > abstract.txt.tag
+    echo "processing $fname ..."
+
+    ss_file="${SS_DIR}/${fname}"
+    tok_file="${TOK_DIR}/${fname}"
+    parse_file="${PARSE_DIR}/${fname}"
+    sdep_file="${SDEP_DIR}/${fname}"
+    sdepcc_file="${SDEPCC_DIR}/${fname}"
+    tag_file="${TAG_DIR}/${fname}"
+
+    cat $f | $GENIASS 1> $ss_file 2> /dev/null
+    cat $ss_file | $TOKENIZER 1> $tok_file 2> /dev/null
+    cat $ss_file | $BLLIP 1> $parse_file 2> /dev/null
+    cat $parse_file | $SDEP 1> $sdep_file 2> /dev/null
+    cat $parse_file | $SDEP -c 1> $sdepcc_file 2> /dev/null
+    cat $ss_file | $GENIA 1> $tag_file 2> /dev/null
+done
