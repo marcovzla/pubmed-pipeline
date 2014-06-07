3ab48ddfc7c75e88b4dc4a44812aa05037c58367
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2014-06-07 12:54:30 -0700

    don't use geniattager tokenization

--- src/tagger.sh
+++ src/tagger.sh
@@ -11,6 +11,6 @@ TMP_INPUT=`mktemp`
 trap 'rm -f "${TMP_INPUT}"' EXIT INT TERM HUP
 cat > "${TMP_INPUT}"
 pushd $GENIA_DIR > /dev/null
-cat "${TMP_INPUT}" | ./geniatagger
+cat "${TMP_INPUT}" | ./geniatagger -nt  # input should be already tokenized
 rm -rf "${TMP_INPUT}"
 popd > /dev/null
